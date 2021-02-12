0ee996805ceaaf4da85e4724ec16d58b4b102de1
babyazalea <usun12@hotmail.com>
2021-02-12 23:10:17 +0900

    Observing Types of Response

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
 
 import { Post } from "./post.model";
 
@@ -16,7 +21,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         "https://angular-complete-7851f-default-rtdb.firebaseio.com/posts.json",
-        postData
+        postData,
+        {
+          observe: "response",
+        }
       )
       .subscribe(
         (responseData) => {
@@ -57,8 +65,22 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete(
-      "https://angular-complete-7851f-default-rtdb.firebaseio.com/posts.json"
-    );
+    return this.http
+      .delete(
+        "https://angular-complete-7851f-default-rtdb.firebaseio.com/posts.json",
+        {
+          observe: "events",
+        }
+      )
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
