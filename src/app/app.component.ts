d0b8be834c7e3eb80a3042228bb00c4ac3bbbaba
babyazalea <usun12@hotmail.com>
2021-02-12 21:49:11 +0900

    error handling with Subject in Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
 
 import { Post } from "./post.model";
 import { PostsService } from "./post.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postService: PostsService) {}
 
   ngOnInit() {
+    this.errorSub = this.postService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
+    });
+
     this.isFetching = true;
     this.postService.fetchPosts().subscribe(
       (posts) => {
@@ -55,5 +60,9 @@ export class AppComponent implements OnInit {
     this.postService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
 
 import { Post } from "./post.model";
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
+  error = new Subject<string>();
+
   constructor(private http: HttpClient) {}
 
   createAndStroePost(title: string, content: string) {
@@ -15,9 +18,14 @@ export class PostsService {
         "https://angular-complete-7851f-default-rtdb.firebaseio.com/posts.json",
         postData
       )
-      .subscribe((responseData) => {
-        console.log(responseData);
-      });
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
   }
 
   fetchPosts() {
